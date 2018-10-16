402f2abdf9a82ce1e645ed82286c8edf64eceb4c
vmohir <vahidm1467@gmail.com>
2018-10-16 14:13:42 +0330

    add platforms to gitignore

--- platforms/android/app/build/intermediates/assets/release/app/app.routing.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var list_component_1 = require("./views/list/list.component");
-var simple_game_component_1 = require("./views/simple-game/simple-game.component");
-exports.routes = [{ path: 'list', component: list_component_1.ListComponent }, { path: '', component: simple_game_component_1.SimpleGameComponent }];
-exports.navigatableComponents = [list_component_1.ListComponent, simple_game_component_1.SimpleGameComponent];
--- platforms/android/app/build/intermediates/assets/release/app/main.aot.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_static_1 = require("nativescript-angular/platform-static");
-var app_module_ngfactory_1 = require("./app.module.ngfactory");
-platform_static_1.platformNativeScript().bootstrapModuleFactory(app_module_ngfactory_1.AppModuleNgFactory);
